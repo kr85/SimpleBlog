4450e92eaa5e6f5572761c2f6bbc9479c68f1253
Kosta Rashev <ni6to.bg@gmail.com>
2014-06-20 19:28:00 -0700

    Added Brazilian Portuguese translation. Made the blog bilingual.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
     @article = current_user.articles.new(article_params)
     respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
+        format.html { redirect_to @article, notice: t('articles.create_success') }
         format.json { render :show, status: :created, location: @article }
       else
         format.html { render :new }
@@ -44,8 +44,8 @@ class ArticlesController < ApplicationController
     @article = current_user.articles.find(params[:id])
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
+        format.html { redirect_to @article, notice: t('articles.update_success') }
+        format.json { head :no_content }
       else
         format.html { render :edit }
         format.json { render json: @article.errors, status: :unprocessable_entity }
@@ -59,7 +59,7 @@ class ArticlesController < ApplicationController
     @article = current_user.articles.find(params[:id])
     @article.destroy
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+      format.html { redirect_to articles_url, notice: t('articles.destroy_success') }
       format.json { head :no_content }
     end
   end
@@ -67,7 +67,7 @@ class ArticlesController < ApplicationController
   def notify_friend
     @article = Article.find(params[:id])
     Notifier.email_friend(@article, params[:name], params[:email]).deliver
-    redirect_to @article, :notice => "Successfully sent a message to your friend."
+    redirect_to @article, :notice => t('articles.notify_friend_success')
   end
 
   private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,20 @@
 module ApplicationHelper
   # Creates a submit button with given name with a cancel link
   # Accepts two arguments: Form object and the cancel link name
-  def submit_or_cancel(form, name='Cancel')
-    form.submit + " or " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
+  def submit_or_cancel(form, name=t('general.cancel'))
+    form.submit + " " + t('general.or') + " " + link_to(name, 'javascript:history.go(-1);', :class => 'cancel')
+  end
+  
+  def language_selector
+    if I18n.locale == :en
+      link_to "PT", url_for(:locale => 'pt-br')
+      link_to "BG", url_for(:locale => 'bg')
+    elsif I18n.locale == :bg
+      link_to "PT", url_for(:locale => 'pt-br')
+      link_to "EN", url_for(:locale => 'en')
+    else
+      link_to "EN", url_for(:locale => 'en')
+      link_to "BG", url_for(:locale => 'bg')
+    end
   end
 end
